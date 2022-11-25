c0e4e63c3ffedeb52d4d74cdc8f80ebdcb417b26
ayeoniii <119025488+ayeoniii@users.noreply.github.com>
2022-11-25 14:04:29 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,28 +1,44 @@
-let YURI;
-function setup()
+//let YURI;
+//function setup()
 {
-    soundFormats('mp3', 'ogg');
-    YURI = loadSound('Blank',loadMusic);
-    //YURI.play();
+   // soundFormats('mp3', 'ogg');
+   // YURI = loadSound('Blank',loadMusic);
+   // YURI.play();
 }
 
 
-function loadMusic()
+//function loadMusic()
 {
-    YURI.play(); //
+   // YURI.play(); //
 }
 
 function setup()
 {
     createCanvas(200,200)
     button = createButton('play');
+    song = loadSound('Blank',loaded);
     button.mousePressed(togglePlaying);
     background(51);
 }
 
-function draw()
+function togglePlaying(){
+    if(!song.isPlaying()){
+        song.play();
+        song.setVolume(0.3);
+        button.html('pause');
+    } else {
+        song.stop();
+        button.html('play');
+    }
+}
+
+ funtion loaded(){
+     console.log(loaded');
+     }
+    
+//function draw()
 {
-    fill(180,0,0);
-    ellipse(50,50,50,50);
+    //fill(180,0,0);
+    //ellipse(50,50,50,50);
 
 }
